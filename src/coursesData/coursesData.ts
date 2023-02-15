a79cd46c1970c99f07d1a43b2ca6f250c62824f0
Braian Fellipe <braianfelipecsk123@hotmail.com>
2023-02-15 11:43:29 -0300

    Inserção dos cursos no rota de cursos

--- src/coursesData/coursesData.ts
+++ src/coursesData/coursesData.ts
@@ -8,4 +8,108 @@ export const courses = [
             amount:100*297, //100 = R$1,00 * quantos reais você deseja
             image_url:"",
         },
+        {
+            title:"ATUALIZAÇÃO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE ESCOLARES",
+            slug:"atualizacao-para-condutores-de-veiculos-de-transporte-de-escolares",
+            codigoCurso:2,
+            code:"2",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"ATUALIZAÇÃO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE COLETIVO DE PASSAGEIROS",
+            slug:"atualizacao-para-condutores-de-veiculos-de-transporte-coletivo-de-escolares-passageiros",
+            codigoCurso:3,
+            code:"3",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"ATUALIZAÇÃO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE CARGAS DE PRODUTOS PERIGOSOS",
+            slug:"atualizacao-para-condutores-de-veiculos-de-transporte-de-cargas-de-produtos-perigosos",
+            codigoCurso:4,
+            code:"4",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"ATUALIZAÇÃO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE EMERGÊNCIA",
+            slug:"atualizacao-para-condutores-de-veiculos-de-transporte-de-emergenia",
+            codigoCurso:5,
+            code:"5",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"ATUALIZAÇÃO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE CARGAS INDIVISÍVEIS",
+            slug:"atualizacao-para-condutores-de-veiculos-de-transporte-de-cargas-indivisiveis",
+            codigoCurso:6,
+            code:"6",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE CARGA INDIVISÍVEL E OUTRAS",
+            slug:"curso-para-condutores-de-veiculo-de-transporte-de-carga-indivisivel-e-outras",
+            codigoCurso:7,
+            code:"7",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE PRODUTOS PERIGOSOS",
+            slug:"curso-para-condutores-de-veiculo-de-transporte-de-produtos-perigosos",
+            codigoCurso:8,
+            code:"8",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE COLETIVO",
+            slug:"curso-para-condutores-de-veiculo-de-transporte-coletivo",
+            codigoCurso:9,
+            code:"9",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE ESCOLARES",
+            slug:"curso-para-condutores-de-veiculo-de-transporte-de-escolares",
+            codigoCurso:10,
+            code:"10",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO PARA CONDUTORES DE VEÍCULO DE TRANSPORTE DE EMERGÊNCIA",
+            slug:"curso-para-condutores-de-veiculo-de-transporte-de-emergencia",
+            codigoCurso:11,
+            code:"11",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"PRIMEIRA HABILITAÇÃO ONLINE – AULAS REMOTAS EAD",
+            slug:"primeira-habilitacao-online",
+            codigoCurso:14,
+            code:"14",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO DE ATUALIZAÇÃO PARA RENOVAÇÃO DA CNH",
+            slug:"curso-de-atualizacao-para-renovacao-da-cnh",
+            codigoCurso:16,
+            code:"16",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
+        {
+            title:"CURSO PREVENTIVO DE RECICLAGEM",
+            slug:"curso-preventivo-de-reciclagem",
+            codigoCurso:17,
+            code:"17",
+            amount:100*297, //100 = R$1,00 * quantos reais você deseja
+            image_url:"",
+        },
     ]
