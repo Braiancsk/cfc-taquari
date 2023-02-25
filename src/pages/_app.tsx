947ff21f9f9408d4f7cee2898f708ab3ae0e2e5f
Braian Fellipe <braianfelipecsk123@hotmail.com>
2023-02-25 17:47:10 -0300

    Continuação de blog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import Link from 'next/link'
-import { PrismicProvider } from '@prismicio/react'
+import { PrismicLink, PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { repositoryName } from "../../prismicio"
 import { Heading } from '@/components/Heading/Heading'
@@ -16,27 +16,52 @@ const inter = Inter({ subsets: ['latin'] })
 const queryClient = new QueryClient()
 
 const richTextComponents = {
-  heading1:({children}:any) => {
-    <Heading as="h2" size='3xl' className='mb-7 mt-12'>
+  heading1: ({ children }:any) => (
+    <Heading as="h2" size="3xl" className="mb-7 mt-12 first:mt-0 last:mb-0">
       {children}
     </Heading>
-  },
-  heading2:({children}:any) => {
-    <Heading as="h3" size='2xl' className='my-2'>
+  ),
+  heading2: ({ children }:any) => (
+    <Heading as="h3" size="2xl" className="mb-7 last:mb-0">
       {children}
     </Heading>
-  },
-  heading3:({children}:any) => {
-    <Heading as="h4" size="2xl" className='my-2'>
+  ),
+  heading3: ({ children }:any) => (
+    <Heading as="h4" size="xl" className="mb-7 last:mb-0">
       {children}
     </Heading>
-  },
-  paragraph:({children}:any) => {
-    <p className='text-title mb-4'>
+  ),
+  paragraph: ({ children }:any) => <p className="mb-7 last:mb-0">{children}</p>,
+  oList: ({ children }:any) => (
+    <ol className="mb-7 pl-4 last:mb-0 md:pl-6">{children}</ol>
+  ),
+  oListItem: ({ children }:any) => (
+    <li className="mb-1 list-decimal pl-1 last:mb-0 md:pl-2">{children}</li>
+  ),
+  list: ({ children }:any) => (
+    <ul className="mb-7 pl-4 last:mb-0 md:pl-6">{children}</ul>
+  ),
+  listItem: ({ children }:any) => (
+    <li className="mb-1 list-disc pl-1 last:mb-0 md:pl-2">{children}</li>
+  ),
+  preformatted: ({ children }:any) => (
+    <pre className="mb-7 rounded bg-slate-100 p-4 text-sm last:mb-0 md:p-8 md:text-lg">
+      <code>{children}</code>
+    </pre>
+  ),
+  strong: ({ children }:any) => (
+    <strong className="font-semibold">{children}</strong>
+  ),
+  hyperlink: ({ children, node }:any) => (
+    <PrismicLink
+      field={node.data}
+      className="underline decoration-1 underline-offset-2"
+    >
       {children}
-    </p>
-  },
-}
+    </PrismicLink>
+  ),
+};
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -46,7 +71,10 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${inter.style.fontFamily};
         }
       `}</style>
-        <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+        <PrismicProvider 
+        internalLinkComponent={(props) => <Link {...props} />} 
+        richTextComponents={richTextComponents}
+        >
           <PrismicPreview repositoryName={repositoryName}>
             <Component {...pageProps} />
           </PrismicPreview>
